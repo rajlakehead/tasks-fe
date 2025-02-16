c53bf5f644db2d3bfe9680fdf4bb75fead41481a
Raj Patel <patelraj24365@gmail.com>
2025-02-16 00:11:08 -0500

    deployment fix

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -170,7 +170,7 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const API_URL = "https://spring-app-ev1u.onrender.com";
+  const API_URL = import.meta.env.VITE_BACKEND_URL;;
 
   const jsonHeaders = {
     headers: { "Content-Type": "application/json" },
